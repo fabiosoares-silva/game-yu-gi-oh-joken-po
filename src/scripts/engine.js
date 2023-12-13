dd828c13409ffd5baf0a6bb6a169fc5f8b1c6797
fabiosoares-silva <fabiosores.soares@gmail.com>
2023-12-13 11:49:57 -0300

    Criar enumeração e local de armazenamento das cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -22,6 +22,36 @@ const players = {
     player1: 'player-cards',
 };
 
+const pathImage = '.src/assets/icons/';
+
+const cardData = [
+    {
+        id: 0,
+        name: 'Blue Eyes White Dragon',
+        type: 'Paper',
+        img: `${pathImage}dragon.png`,
+        winOf: [1],
+        loseOf: [2],
+    },
+
+    {
+        id: 1,
+        name: 'Dark Magician',
+        type: 'Rock',
+        img: `${pathImage}magician.png`,
+        winOf: [2],
+        loseOf: [0],
+    },
+    {
+        id: 2,
+        name: 'Exodia',
+        type: 'Scissors',
+        img: `${pathImage}exodia.png`,
+        winOf: [0],
+        loseOf: [1],
+    },
+];
+
 function init() {}
 
 init();
